0df524a0badd8b945bafb03ac054a1234eca236c
IgorShaposhnikov <Igor.Shaposhnikov@urfu.me>
2026-06-28 00:21:49 +0500

    feat: add asp net core support

---
+++ src/Connexus.AspNetCore/ModuleLifecycleHostedService.cs
@@ -0,0 +1,30 @@
+﻿using Connexus.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Connexus.AspNetCore;
+
+internal sealed class ModuleLifecycleHostedService(IServiceProvider serviceProvider) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var modules = ModuleRegistrationExtensions.GetSortedModules();
+        using var scope = serviceProvider.CreateScope();
+
+        foreach (var module in modules)
+        {
+            await module.OnStartedAsync(scope.ServiceProvider);
+        }
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        var modules = ModuleRegistrationExtensions.GetSortedModules();
+        using var scope = serviceProvider.CreateScope();
+
+        foreach (var module in modules)
+        {
+            await module.OnStoppingAsync(scope.ServiceProvider);
+        }
+    }
+}
